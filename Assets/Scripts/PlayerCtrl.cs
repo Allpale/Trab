4433962c60cebdb67c8404190c14c7a299d9c0ac
Allan P. Leme <allpale16@gmail.com>
2018-07-27 09:22:51 -0300

    Double Jump

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -37,6 +37,9 @@ public class PlayerCtrl : MonoBehaviour {
 	public bool isGrounded;
 	public LayerMask whatIsGround;
 
+	bool canDoubleJump = false;
+	public float delayForDoubleJump = 0.2f;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
@@ -104,7 +107,21 @@ public class PlayerCtrl : MonoBehaviour {
 		isJumping = true;
 		rb.AddForce(new Vector2(0f ,jumpSpeed));
 		anim.SetInteger("Stats", 8);
+
+		Invoke("EnableDoubleJump", delayForDoubleJump);
 		}
+
+		if (canDoubleJump && !isGrounded) {
+			rb.velocity = Vector2.zero;
+			rb.AddForce(new Vector2(0f, jumpSpeed));
+			anim.SetInteger("State", 1);
+			canDoubleJump = false;
+		}
+
+	}
+
+	void EnableDoubleJump() {
+		canDoubleJump = true;
 	}
 
 	void OnCollisionEnter2D(Collision2D other) {
